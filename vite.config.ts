cb6982ee3e88537e7590d3508217886f60ed04d6
petrixia <crisdev@duck.com>
2025-07-07 14:59:14 -0300

    fix: update Vite configuration to include build options

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
+import { defineConfig } from 'vite';
 export default defineConfig({
-  plugins: [react()],
-})
+  // outras configs...
+  build: {
+    rollupOptions: {
+      // garantir que assets como SVGs sejam resolvidos corretamente
+      input: 'index.html',
+    },
+  },
+});
